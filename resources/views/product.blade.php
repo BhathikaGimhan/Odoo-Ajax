b28ce9d8fa8caba3ae906839eca152e767ef16f8
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-04-12 18:25:51 +0530

    add bootstrap

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,6 +3,10 @@
 <html>
 <head>
     <title>Create Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Create Product</h1>
@@ -11,36 +15,36 @@
 
 <form method="POST" action="{{ route('product.store') }}" id="create-product-form">
     @csrf
-    <div>
+    <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" name="name" id="name">
+        <input class="form-control" type="text" name="name" id="name">
     </div>
-    <div>
+    <div class="form-group">
         <label for="default_code">Default Code:</label>
-        <input type="text" name="default_code" id="default_code">
+        <input class="form-control" type="text" name="default_code" id="default_code">
     </div>
-    <div>
+    <div class="form-group">
         <label for="list_price">List Price:</label>
-        <input type="number" name="list_price" id="list_price" step="0.01">
+        <input class="form-control" type="number" name="list_price" id="list_price" step="0.01">
     </div>
-    <div>
+    <div class="form-group">
         <label for="standard_price">Standard Price:</label>
-        <input type="number" name="standard_price" id="standard_price" step="0.01">
+        <input class="form-control" type="number" name="standard_price" id="standard_price" step="0.01">
     </div>
-    <button type="submit" id="create-product-btn">Create</button>
+    <button class="btn btn-outline-primary" type="submit" id="create-product-btn">Create</button>
 </form>
-<table>
-    <thead>
+<table class="table table-hover table-inverse table-responsive">
+    <thead class="thead-inverse">
         <tr>
             <th>Name</th>
             <th>Default Code</th>
             <th>List Price</th>
             <th>Standard Price</th>
         </tr>
-    </thead>
-    <tbody id="product-data">
-        <!-- Product data will be loaded here -->
-    </tbody>
+        </thead>
+        <tbody id="product-data">
+            <!-- Product data will be loaded here -->
+        </tbody>
 </table>
 <p id="text"></p>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
